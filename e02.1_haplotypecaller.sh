76a8d0ab7ebd1f46d0718dcd2f6203fbe5b5e7c1
Mohamed <mohammed.elmanzalawi@gmail.com>
2025-05-21 12:32:09 +0900

    Defined variables for  result directory paths

--- e02.1_haplotypecaller.sh
+++ e02.1_haplotypecaller.sh
@@ -37,12 +37,18 @@ conda deactivate
 NUM=$SLURM_ARRAY_TASK_ID
 NAME=$(cat $sample_ids | head -$NUM | tail -1)
 
+# Set up directories
 fastp_result=${output_dir}/00_samples/${NAME}/00_fastp_results
 haplotype_output=${output_dir}/00_samples/${NAME}/01_haplotype_result
+bam_dir=${haplotype_output}/00_bam_files
+bam_after_marks=${haplotype_output}/01_bam_after_marks
+marksduplicate_metrics=${haplotype_output}/02_marksduplicate_metrics
+BQSR_report=${haplotype_output}/03_BQSR_report
+bam_after_bqsr_results=${haplotype_output}/04_after_bqsr_results
+gvcf_output=${output_dir}/01_gvcf
 
-
-for dir in ${temp_dir} ${haplotype_output}/00_bam_files/ ${haplotype_output}/01_bam_after_marks/ ${haplotype_output}/02_marksduplicate_metrics/ \
-${haplotype_output}/03_BQSR_report/ ${haplotype_output}/04_after_bqsr_results/ ${output_dir}/01_gvcf/
+for dir in ${temp_dir} ${bam_dir} ${bam_after_marks} ${marksduplicate_metrics} \
+${BQSR_report} ${bam_after_bqsr_results} ${gvcf_output}
 do
     mkdir -p ${dir} 
 done
@@ -52,22 +58,22 @@ bwa mem -t 16 -K 10000000 -R "@RG\tID:${NAME}\tPU:${NAME}\tSM:${NAME}\tPL:illumi
 ${Ref} ${fastp_result}/${NAME}_R1.fq ${fastp_result}/${NAME}_R2.fq | \
 singularity exec ${gatk_sif} gatk --java-options -Xmx16g \
 SortSam --MAX_RECORDS_IN_RAM 5000000 -I /dev/stdin \
--O ${haplotype_output}/00_bam_files/${NAME}.germline.bam --SORT_ORDER coordinate --TMP_DIR ${temp_dir}
+-O ${bam_dir}/${NAME}.germline.bam --SORT_ORDER coordinate --TMP_DIR ${temp_dir}
 
 # Mark Duplicates
 singularity exec ${gatk_sif} gatk --java-options -Xmx16g \
             MarkDuplicates \
-            -I ${haplotype_output}/00_bam_files/${NAME}.germline.bam \
-            -O ${haplotype_output}/01_bam_after_marks/${NAME}.germline_dedup.bam \
-            -M ${haplotype_output}/02_marksduplicate_metrics/${NAME}.germline_dedup.bam.metrics.txt \
+            -I ${bam_dir}/${NAME}.germline.bam \
+            -O ${bam_after_marks}/${NAME}.germline_dedup.bam \
+            -M ${marksduplicate_metrics}/${NAME}.germline_dedup.bam.metrics.txt \
             --TMP_DIR ${temp_dir}
 
 # Generate BQSR Report
 singularity exec ${gatk_sif} gatk --java-options -Xmx16g \
             BaseRecalibrator \
             -R ${Ref} \
-            -I ${haplotype_output}/01_bam_after_marks/${NAME}.germline_dedup.bam \
-            -O ${haplotype_output}/03_BQSR_report/${NAME}.germline_recal.table \
+            -I ${bam_after_marks}/${NAME}.germline_dedup.bam \
+            -O ${BQSR_report}/${NAME}.germline_recal.table \
             --known-sites ${Ref_dir}/Homo_sapiens_assembly38.dbsnp138.vcf \
             --known-sites ${Ref_dir}/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz 
 
@@ -75,16 +81,16 @@ singularity exec ${gatk_sif} gatk --java-options -Xmx16g \
 singularity exec ${gatk_sif} gatk --java-options -Xmx16g \
             ApplyBQSR \
             -R ${Ref} \
-            -I ${haplotype_output}/01_bam_after_marks/${NAME}.germline_dedup.bam \
-            --bqsr-recal-file ${haplotype_output}/03_BQSR_report/${NAME}.germline_recal.table \
-            -O ${haplotype_output}/04_after_bqsr_results/${NAME}.germline_dedup_bqsr.bam
+            -I ${bam_after_marks}/${NAME}.germline_dedup.bam \
+            --bqsr-recal-file ${BQSR_report}/${NAME}.germline_recal.table \
+            -O ${bam_after_bqsr_results}/${NAME}.germline_dedup_bqsr.bam
 
 
 #Run Haplotype Caller
 singularity exec ${gatk_sif} gatk --java-options -Xmx16g \
             HaplotypeCaller \
-            --input ${haplotype_output}/04_after_bqsr_results/${NAME}.germline_dedup_bqsr.bam \
-            --output ${output_dir}/01_gvcf/${NAME}.germline.g.vcf \
+            --input ${bam_after_bqsr_results}/${NAME}.germline_dedup_bqsr.bam \
+            --output ${gvcf_output}/${NAME}.germline.g.vcf \
             --reference ${Ref} \
             -ERC GVCF \
             --native-pair-hmm-threads 16
