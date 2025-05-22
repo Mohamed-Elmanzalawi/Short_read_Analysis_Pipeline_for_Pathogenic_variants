2918deb97ba24a2a0de57ce1aa40ecdb4912de96
Mohamed <mohammed.elmanzalawi@gmail.com>
2025-05-22 12:44:42 +0900

    Upgraded to use for loop over the samples instead of array jobs to conserve memory.

--- e05.1_var_per_sample.py
+++ e05.1_var_per_sample.py
@@ -84,69 +84,74 @@ def filter_pathogenic_variants(df, n_conditions=5):
     removed_variants = num_of_var - len(df_filtered)
     return df_filtered, removed_variants
     
-def process_sample(annovar_df, output_dir, sample, FILTER_BY_PATHOGENICITY, n_conditions=5):
+def process_sample(annovar_df, output_dir, sample_ids, FILTER_BY_PATHOGENICITY, n_conditions=5):
     """Process a single sample from ANNOVAR output."""
-    df = selecting_sample_columns_only(annovar_df,sample)
-    df['GT']= split_otherinfo_column(df, 0)
-    df['AD']= split_otherinfo_column(df, 1)
-    df['DP']= split_otherinfo_column(df, 2)
-    df['GQ']= split_otherinfo_column(df, 3)
+    with open(sample_ids, 'r') as sample_ids:
+        for sample in sample_ids:
+            sample = sample.strip()
+            logging.info(f"Processing sample: {sample}")
+            df = selecting_sample_columns_only(annovar_df,sample)
+            df['GT']= split_otherinfo_column(df, 0)
+            df['AD']= split_otherinfo_column(df, 1)
+            df['DP']= split_otherinfo_column(df, 2)
+            df['GQ']= split_otherinfo_column(df, 3)
 
-    os.makedirs(f"{output_dir}/{sample}", exist_ok=True)
-    result_by_sample_orignal = f"{output_dir}/{sample}/{sample}_original.txt"
-    df.insert(0, 'sample', sample)
+            os.makedirs(f"{output_dir}/{sample}", exist_ok=True)
+            result_by_sample_orignal = f"{output_dir}/{sample}/{sample}_original.txt"
+            df.insert(0, 'sample', sample)
 
-    # Keeping orginal file
-    df.to_csv(result_by_sample_orignal, index=False, sep="\t")
-    logging.info(f"A copy before filtration was created for sample {sample}")
+            # Keeping orginal file
+            df.to_csv(result_by_sample_orignal, index=False, sep="\t")
+            logging.info(f"A copy before filtration was created for sample {sample}")
 
-    total_var= len(df)
-    logging.info(f"Total num of variants = {total_var} ")
-    logging.info(f"Commencing variant filtration.")
+            total_var= len(df)
+            logging.info(f"Total num of variants = {total_var} ")
+            logging.info(f"Commencing variant filtration.")
 
-    df,num_rv1 = filter_rare_variants(df)
-    logging.info(f"Removed {num_rv1} variants with AF more than 0.01%")
+            df,num_rv1 = filter_rare_variants(df)
+            logging.info(f"Removed {num_rv1} variants with AF more than 0.01%")
 
-    df,num_rv2 = remove_intronic_and_intergenic(df)
-    logging.info(f"Removed {num_rv2} variants that exist in intronic or intergenic regions")
+            df,num_rv2 = remove_intronic_and_intergenic(df)
+            logging.info(f"Removed {num_rv2} variants that exist in intronic or intergenic regions")
 
-    df,num_rv3 = remove_reference_or_unknown_variants(df)
-    logging.info(f"Removed {num_rv3} variants that are the same as the reference or has unknown genotype ['0/0','0|0','./.', '.|.']")
+            df,num_rv3 = remove_reference_or_unknown_variants(df)
+            logging.info(f"Removed {num_rv3} variants that are the same as the reference or has unknown genotype ['0/0','0|0','./.', '.|.']")
 
-    df,num_rv4 = remove_synonymous_variants(df)
-    logging.info(f"Removed {num_rv4} synonymous variants")
-    
-    if FILTER_BY_PATHOGENICITY == True:
-        df,num_rv5 = filter_pathogenic_variants(df, n_conditions)
-        logging.info(f"Filtering variants ...")
-        logging.info(f"Removed {num_rv5} variants that are neither pathogenic nor likely pathogenic in ClinVar or did not pass at least 5 pathogenicity scores")
-    else:
-        logging.info(f"Pipeline filteration by pathogenicity was set to {FILTER_BY_PATHOGENICITY}. Skipping this step ...")
+            df,num_rv4 = remove_synonymous_variants(df)
+            logging.info(f"Removed {num_rv4} synonymous variants")
+            
+            if FILTER_BY_PATHOGENICITY == True:
+                df,num_rv5 = filter_pathogenic_variants(df, n_conditions)
+                logging.info(f"Filtering variants ...")
+                logging.info(f"Removed {num_rv5} variants that are neither pathogenic nor likely pathogenic in ClinVar or did not pass at least 5 pathogenicity scores")
+            else:
+                logging.info(f"Pipeline filteration by pathogenicity was set to {FILTER_BY_PATHOGENICITY}. Skipping this step ...")
 
-    if len(df) == 0:
-        logging.info(f"{sample} had no results after filtering")
-    else:
-        # Filtered file
-        total_af_var= len(df)
-        logging.info(f"Total num of variants after filtration = {total_af_var} ")
-        result_by_sample_filtered = f"{output_dir}/{sample}/{sample}_filt.txt"
-        df.to_csv(result_by_sample_filtered, index=False, sep="\t")
-        logging.info(f"Sample {result_by_sample_filtered} filtered variants file was created.")
-
+            if len(df) == 0:
+                logging.info(f"{sample} had no results after filtering")
+            else:
+                # Filtered file
+                total_af_var= len(df)
+                logging.info(f"Total num of variants after filtration = {total_af_var} ")
+                result_by_sample_filtered = f"{output_dir}/{sample}/{sample}_filt.txt"
+                df.to_csv(result_by_sample_filtered, index=False, sep="\t")
+                logging.info(f"Sample {result_by_sample_filtered} filtered variants file was created.")
+                logging.info(f"Sample {sample} processing completed.")
+                logging.info("=========================================================================================================================================")
     return df
 
 
-def main(annovar_file, output_dir, sample, FILTER_BY_PATHOGENICITY=True, n_conditions=5, num_processes=2):
+def main(annovar_file, output_dir, sample_ids, FILTER_BY_PATHOGENICITY=True, n_conditions=5, num_processes=2):
     """Main function to process a single ANNOVAR file for multiple samples.
        The script will choose the details for sample selected from the ANNOVAR file and output two files: one 
        file before any filtration + another file after filtration"""
     df = read_csv(annovar_file)
-    process_sample(df, output_dir, sample, FILTER_BY_PATHOGENICITY, n_conditions)
+    process_sample(df, output_dir, sample_ids, FILTER_BY_PATHOGENICITY, n_conditions)
 
 if __name__ == "__main__":
     ANNOVAR_ALL_SAMPLES_FILE = sys.argv[1]
     OUTPUT_DIR = sys.argv[2]
-    SAMPLE = sys.argv[3]
+    SAMPLE_IDS = sys.argv[3]
     FILTER_BY_PATHOGENICITY = sys.argv[4]
     PATHOGENICITY_CONDITIONS = sys.argv[5]
-    main(ANNOVAR_ALL_SAMPLES_FILE, OUTPUT_DIR, SAMPLE, FILTER_BY_PATHOGENICITY,int(PATHOGENICITY_CONDITIONS))
+    main(ANNOVAR_ALL_SAMPLES_FILE, OUTPUT_DIR, SAMPLE_IDS, FILTER_BY_PATHOGENICITY,int(PATHOGENICITY_CONDITIONS))
